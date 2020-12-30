69e12b001d81e085b4052593de50774605c9ec9d
dagmaragluch <dagmara320@gmail.com>
2020-12-30 15:45:47 +0100

    add list of ports and reader to this list

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -12,16 +12,8 @@ public class Point {
         return x;
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
     public double getY() {
         return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
     }
 
     public String toString() {
---
+++ src/main/java/PortsReader.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PortsReader {
+
+    public static void main(String[] args) {
+        String fileName = "src/main/resources/ports.txt";
+        Map<String, Point> map = readPorts(fileName);
+    }
+
+
+    public static Map<String, Point> readPorts(String portsFile) {
+
+        BufferedReader br = null;
+        String line;
+        String separator = ",";
+
+        Map<String, Point> portsMap = new HashMap<>();
+
+
+        try {
+            br = new BufferedReader(new FileReader(portsFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] lineAsArray = line.split(separator);
+                String portName = lineAsArray[0];
+                Point location = new Point(Double.parseDouble(lineAsArray[1]), Double.parseDouble(lineAsArray[2]));
+
+                portsMap.put(portName, location);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return portsMap;
+    }
+
+}
